f3955aa391ceb5f373f27e5fa8d574b77e5f0713
Victor <vicvighi8@gmail.com>
2023-08-16 14:39:58 -0300

    Sistema de movimentação plataforma
    Sistema de plataforma funcionando.
    
    Copiei tudo deste tutorial: https://youtu.be/9HAZQROH2gM
    
    Tem variáveis públicas de velocidade e força do pulo junto com a variável de gravidade para controlar a movimentação.

--- Assets/Scripts/radioativeMeatMovement.cs
+++ Assets/Scripts/radioativeMeatMovement.cs
@@ -4,17 +4,81 @@ using UnityEngine;
 
 public class radioativeMeatMovement : MonoBehaviour
 {
-    public float vel;
-    
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform ceillingCheck;
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    public float checkRadius;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private bool isJumping = false;
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //GetInputs
+        ProcessInputs();
+
+        //Animate
+        Animate();
+
+       
+    }
+
+    //Better for handling Physics, can be called multiple times per update frame.
+    private void FixedUpdate()
+    {
+        //check if is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+
+        //move
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+        isJumping = false;
+    }
+
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
